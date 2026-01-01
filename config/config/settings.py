ac03b1a80db29523db54a9fb78e3086a895ea103
Disha Sharma <121336805+DS92428@users.noreply.github.com>
2026-01-02 00:51:26 +0530

    settings.py

--- config/config/settings.py
+++ config/config/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+DEBUG = os.getenv("DJANGO_DEBUG") == "True"
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
